1a66900f593234c1b2ff23a5a629c938810dfe39
Tower95 <torrestmi3@gmail.com>
2021-09-19 20:23:41 -0700

    reto 1

---
+++ utilityHost.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+# Reto 1
+# Desarrollar un programa llamado utilityHost.sh, dentro de él vamos a 
+# declarar dos variables una llamada option, otra llamada result, vamos
+# a inicializarles e imprimir sus valores.
+
+option=1
+result=2
+
+echo "Option = $option y result = $result "
+
